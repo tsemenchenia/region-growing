2df9c43fb417f47ce853e11ef7dd28dfe7712ce8
Yahor_Semianchenia <Yahor_Semianchenia@epam.com>
2020-11-04 22:30:54 +0300

    Runner update

--- src/main/java/com/lts/Runner.java
+++ src/main/java/com/lts/Runner.java
@@ -17,16 +17,19 @@ public class Runner {
 
     public static void main(String[] args) throws IOException {
         ClassLoader classLoader = Runner.class.getClassLoader();
-        URL resource = Objects.requireNonNull(classLoader.getResource("2.jpg"));
-        BufferedImage sourceImage = ImageIO.read(new File(resource.getFile()));
 
-        RegionFinder regionFinder = new RegionFinderImpl();
-        List<List<Point>> regions = regionFinder.findRegions(sourceImage, new Color(255, 255, 255));
+        for (int i = 1; i <= 40; i++) {
+            URL resource = Objects.requireNonNull(classLoader.getResource("IM" + i + ".jpg"));
+            BufferedImage sourceImage = ImageIO.read(new File(resource.getFile()));
 
-        BufferedImage coloredImage = ImageUtil.colorRegions(sourceImage, regions);
+            RegionFinder regionFinder = new RegionFinderImpl();
+            List<List<Point>> regions = regionFinder.findRegions(sourceImage, new Color(255, 255, 255));
 
-        File processedImageFile = new File("2-processed.jpg");
-        ImageIO.write(coloredImage, "jpg", processedImageFile);
+            BufferedImage coloredImage = ImageUtil.colorRegions(sourceImage, regions);
+
+            File processedImageFile = new File("IM" + i + "-processed.jpg");
+            ImageIO.write(coloredImage, "jpg", processedImageFile);
+        }
     }
 
 }
